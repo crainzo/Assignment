450b23d6047a004982167775d6dbe7c036f6e317
raaz <crainzo9t@gmail.com>
2022-02-18 14:03:32 +0530

    commit made for branch 2

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 SEARCH_STRING="ERROR"
-FILE_NAME="dpkg.log"
-cd /var/log
+FILE_NAME="alternatives.log"
+cd /var/log/
 
-OUTPUT=$(grep -i $SEARCH_STRING $FILE_NAME)
+OUTPUT=$(grep -i $SEARCH_STRING)
+echo $OUTPUT
 $OUTPUT >> /home/crainzo/git_project/OUTPUT_LOG.txt
+
+
 
 
 
